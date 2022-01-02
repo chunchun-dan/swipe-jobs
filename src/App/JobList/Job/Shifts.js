3d21c28ef062476f25b970b84d2ae2a3c7eb5eee
dcc8233299 <cdan199221@gmail.com>
2022-01-02 19:56:46 +1100

    add formatted shift dates

--- src/App/JobList/Job/Shifts.js
+++ src/App/JobList/Job/Shifts.js
@@ -18,9 +18,18 @@ const Shifts = ({
       <div>
         {'Shift Dates'}
         {shifts.map((o, index) => {
+          const startDate = new Date(o.startDate);
+          const [startWeekday, startMonth, startDay] = startDate.toDateString().split(' ', 3);
+          const startHour = startDate.toLocaleTimeString('en-US',{ hour: 'numeric', minute: 'numeric', hour12: true });
+          const endDate = new Date(o.endDate);
+          const [endWeekday, endMonth, endDay] = endDate.toDateString().split(' ', 4);
+          const endHour = endDate.toLocaleTimeString('en-US',{ hour: 'numeric', minute: 'numeric', hour12: true });
+          const formattedDate = startDay === endDay
+          ? `${startMonth.toUpperCase()} ${startDay.toUpperCase()}, ${startWeekday.toUpperCase()} ${startHour} - ${endHour} PDT`
+          : `${startMonth.toUpperCase()} ${startDay.toUpperCase()}, ${startWeekday.toUpperCase()} ${startHour} - ${endMonth.toUpperCase()} ${endDay.toUpperCase()}, ${endWeekday.toUpperCase()} ${endHour} PDT`;
           return(
             <div key={index}>
-              {'date'}
+              {formattedDate}
             </div>
           )
         })}
