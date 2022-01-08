bd8d7fc671f5720c6716ba0a7c2a22eb7bab8640
dcc8233299 <cdan199221@gmail.com>
2022-01-08 22:22:32 +1100

    add style to ReportTo

--- src/App/JobList/Job/ReportTo.js
+++ src/App/JobList/Job/ReportTo.js
@@ -1,5 +1,6 @@
 // @flow
 import * as React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
 type Props = {
   name: string,
@@ -10,17 +11,34 @@ const ReportTo = ({
   name,
   phone,
 }: Props): React.Node => {
+  const formattedPhoneNumber = phone?.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2 $3");
+  const styles = StyleSheet.create({
+    container: {
+      display: 'flex',
+      margin: '16px 0 16px 0',
+      alignItems: 'center',
+    },
+    icon: {
+      fontSize: '20px',
+      margin: '0 24px 0 24px',
+    },
+    label: {
+      fontWeight: 'bold',
+      marginBottom: '8px',
+    }
+  })
+
   return(
-    <div>
-      <div>
+    <div className={css(styles.container)}>
+      <div className={css(styles.icon)}>
         <i className="fas fa-user-circle"></i>
       </div>
       <div>
-        <div>
+        <div className={css(styles.label)}>
           {'Report To'}
         </div>
         <div>
-          {`${name} ${phone ?? ''}`}
+          {`${name} ${formattedPhoneNumber ?? ''}`}
         </div>
       </div>
     </div>
